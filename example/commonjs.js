eb2032544b70b0506aa935cab7d3424ed4bd2811
Nathachai Thongniran <inid3a@gmail.com>
2022-04-12 11:03:53 +0700

    docs(example): update example in example/commonjs.js

--- example/commonjs.js
+++ example/commonjs.js
@@ -1,5 +1,19 @@
-const filter = require('leo-profanity');
+const filter = require('leo-profanity')
 
-const str = 'I have boob'
-const filteredStr = filter.clean(str, '+', 2)
-console.log(str, filteredStr)
+// in this state,
+// the "filter" only have add "english" dictionary by default
+console.log(filter.list().length)
+
+// remove all bad words from the filter
+// now the filter can't filter anything cause there are no bad words
+filter.clearList()
+console.log(filter.list().length)
+
+// adding word (from builtin dictionary) into the filter
+filter.add(filter.getDictionary('en'))
+filter.add(filter.getDictionary('fr'))
+filter.add(filter.getDictionary('ru'))
+
+// now in this state
+// the filter includes all bad words (3 languages)
+console.log(filter.list().length)
